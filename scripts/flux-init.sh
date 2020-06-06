2f2b87b47443901cd0604f7e1d2b2647ef720892
Steven Wade <steven@stevenwade.co.uk>
2020-06-06 01:39:03 +0100

    Updating README.

--- scripts/flux-init.sh
+++ scripts/flux-init.sh
@@ -20,7 +20,7 @@ fi
 REPO_GIT_INIT_PATHS="kustomize/dev"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 REPO_URL=${1:-git@github.com:swade1987/gitops-with-kustomize}
-REPO_BRANCH=skeleton
+REPO_BRANCH=master
 TEMP=${REPO_ROOT}/temp
 
 rm -rf ${TEMP} && mkdir ${TEMP}
